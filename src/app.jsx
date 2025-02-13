6d451168117e78802411baf25fdc1e20445c6be3
Karly James <kjames1a@byu.edu>
2025-02-12 21:01:03 -0700

    Updated navlinks, imports, and routes.

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { BlindBoxes } from './blindboxes/blindboxes';
+import { Collection } from './collection/collection';
+import { About } from './about/about';
+import { Chat } from './chat/chat';
+
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="body bg-dark text-light"> 
         <header className="container-fluid">
         <h2 className="schoolbell-regular" style={{ marginTop: '15px' }}>Pop Depot</h2>
@@ -16,19 +24,19 @@ export default function App() {
                     <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
             <menu className="navbar-nav d-flex flex-row justify-content-center w-100">
             <li className="nav-item">
-                <a className=" nav-link active schoolbell-regular" href="index.html" style={{ fontSize: '30px', marginBottom: '50px' }}>Home</a>
+                <NavLink className=" nav-link schoolbell-regular" to="/" style={{ fontSize: '30px', marginBottom: '50px' }}>Home</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link schoolbell-regular" href="BlindBoxes.html" style={{ fontSize: '30px'}}>Blind Boxes</a>
+                <NavLink className="nav-link schoolbell-regular" to="BlindBoxes" style={{ fontSize: '30px'}}>Blind Boxes</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link schoolbell-regular " href="Collection.html" style={{ fontSize: '30px'}}>Figurine Collection</a>
+                <NavLink className="nav-link schoolbell-regular " to="Collection" style={{ fontSize: '30px'}}>Figurine Collection</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link schoolbell-regular" href="chat.html" style={{ fontSize: '30px'}}>Chat</a>
+                <NavLink className="nav-link schoolbell-regular" to="chat" style={{ fontSize: '30px'}}>Chat</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link schoolbell-regular" href="about.html" style={{ fontSize: '30px'}}>About</a>
+                <NavLink className="nav-link schoolbell-regular" to="about" style={{ fontSize: '30px'}}>About</NavLink>
             </li>
             </menu>
         </div>
@@ -36,9 +44,14 @@ export default function App() {
         </nav>
         </header>
 
-        <main className="container-fluid bg-light text-center"> 
-            App components go here
-        </main>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/blindboxes' element={<BlindBoxes />} />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/collection' element={<Collection />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
 
         <footer className="text-dark-50">
         <div className="container-fluid">
@@ -47,5 +60,13 @@ export default function App() {
         </div>
         </footer>
     </div>
+    </BrowserRouter>
     );
 }
+
+
+
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
