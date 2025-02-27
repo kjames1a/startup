e841061bccd8ca995442f094936ed6ff03fb6833
Karly James <kjames1a@byu.edu>
2025-02-26 19:36:25 -0700

    Code clean up

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { useState, useEffect } from 'react';
 import './about.css';
 import Accordion from 'react-bootstrap/Accordion';
 
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { NavLink } from "react-router-dom";
-
 import { Unauthenticated } from './unauthenticated';
 import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
@@ -27,25 +25,3 @@ export function Login({ userName, authState, onAuthChange }) {
     </main>
   );
 }
-
-// export function Login() {
-//   return (
-//     <main className="container-fluid bg-light text-center">
-//     <div>
-//       <h1 className="schoolbell-header">Sign in or Register</h1>
-//       <form method="get" action="play.html">
-//         <div className="input-group mb-3">
-//           <span className="input-group-text">@</span>
-//           <input className="form-control" type="text" placeholder="your@email.com" />
-//         </div>
-//         <div className="input-group mb-3">
-//           <span className="input-group-text"><img src="lock.png" width="18"></img></span>
-//           <input className="form-control" type="password" placeholder="password" />
-//         </div>
-//         <NavLink type="submit" style={{ marginRight: "15px" }} className="handdrawn-button" to="/blindboxes">Sign in</NavLink>
-//         <NavLink type="submit" style={{ marginRight: "15px" }} className="secondary-handdrawn-button" to="/blindboxes">Register</NavLink>
-//       </form>
-//     </div>
-//   </main>
-//   );
-// }
