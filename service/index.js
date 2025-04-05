dedf8a8c64068eb54a901b7b13647cdd14120f20
Karly James <kjames1a@byu.edu>
2025-04-05 15:00:50 -0600

    Updated to work with db

--- service/index.js
+++ service/index.js
@@ -3,30 +3,19 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
-// The scores, users, and blind boxes are saved in memory and disappear whenever the service is restarted.
-let blindBoxes = [];
-let users = [];
-
-// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-// JSON body parsing using built-in middleware
 app.use(express.json());
-
-// Use the cookie parser middleware for tracking authentication tokens
 app.use(cookieParser());
-
-// Serve up the front-end static content hosting
 app.use(express.static('public'));
 
-// Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
   if (await findUser('email', req.body.email)) {
     res.status(409).send({ msg: 'Existing user' });
@@ -38,12 +27,12 @@ apiRouter.post('/auth/create', async (req, res) => {
   }
 });
 
-// GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
   const user = await findUser('email', req.body.email);
   if (user) {
     if (await bcrypt.compare(req.body.password, user.password)) {
       user.token = uuid.v4();
+      await DB.updateUser(user);
       setAuthCookie(res, user.token);
       res.send({ email: user.email });
       return;
@@ -52,17 +41,16 @@ apiRouter.post('/auth/login', async (req, res) => {
   res.status(401).send({ msg: 'Unauthorized' });
 });
 
-// DeleteAuth logout a user
 apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
     delete user.token;
+    DB.updateUser(user);
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
 });
 
-// Middleware to verify that the user is authorized to call an endpoint
 const verifyAuth = async (req, res, next) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
@@ -72,53 +60,43 @@ const verifyAuth = async (req, res, next) => {
   }
 };
 
-// GetBlindBoxes
-apiRouter.get('/blindboxes', verifyAuth, (_req, res) => {
-  res.send(blindBoxes);
+apiRouter.get('/collection', verifyAuth, async (req, res) => {
+  const token = req.cookies.token;
+  const collection = await DB.getCollectionFigures(token);
+  res.json(collection);
 });
 
-// AddBlindBox
-apiRouter.post('/blindboxes', verifyAuth, (req, res) => {
-  const newBlindBox = req.body;
-  blindBoxes = updateBlindBoxes(newBlindBox);
-  res.send(blindBoxes);
+apiRouter.post('/collection', verifyAuth, async (req, res) => {
+  const newFigure = req.body;
+  const figure = await DB.updateCollection(newFigure);
+  res.json(figure);
 });
 
-// Default error handler
 app.use(function (err, req, res, next) {
   res.status(500).send({ type: err.name, message: err.message });
 });
 
-// Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-function updateBlindBoxes(newBlindBox) {
-  blindBoxes.push(newBlindBox);
-  return blindBoxes;
-}
 
 async function createUser(email, password) {
   const passwordHash = await bcrypt.hash(password, 10);
-
-  const user = {
-    email: email,
-    password: passwordHash,
-    token: uuid.v4(),
-  };
-  users.push(user);
-
+  const user = { email, password: passwordHash, token: uuid.v4() };
+  await DB.addUser(user);
   return user;
 }
 
 async function findUser(field, value) {
   if (!value) return null;
 
-  return users.find((u) => u[field] === value);
+  if (field === 'token') {
+    return DB.getUserByToken(value);
+  }
+  return DB.getUser(value);
 }
 
-// setAuthCookie in the HTTP response
 function setAuthCookie(res, authToken) {
   res.cookie(authCookieName, authToken, {
     secure: true,
@@ -127,6 +105,9 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.listen(port, () => {
+
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
