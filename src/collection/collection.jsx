b0bdd61a8350891fe66f25c1dda5b773a1b73b05
Karly James <kjames1a@byu.edu>
2025-04-05 15:04:09 -0600

    useState and useEffect updated

--- src/collection/collection.jsx
+++ src/collection/collection.jsx
@@ -1,36 +1,67 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useCollection } from './figureStorage';
 import './collection.css';
 
-export function Collection() {
-  const { collection, addFigure, removeFigure } = useCollection();
+export function Collection(props) {
+  const [collection, setCollection] = React.useState([]);
+  const [selectedBlindBox, setSelectedBlindBox] = React.useState(null); 
+  const userName = props.userName;
 
-  return (
-    <main>
-      <h2 className="schoolbell-regular" style={{ textAlign: "center", marginBottom: "40px", color: "black" }}>
-        Your Figurine Collection
-      </h2>
-      <div className="shelf">
-        <div className="figures">
-          {collection.length === 0 ? (
-            <p style={{ textAlign: "center", color: "gray" }}>No figures collected yet!</p>
-          ) : (
-            collection.map((figure, index) => (
-              <div className="figure" key={index}>
-                <img src={figure.image} alt={figure.name} />
-                {/* Uncomment the following lines if you want to add remove functionality */}
-                {/* <p>{figure.name}</p> */}
-                {/* <button className='remove-button' onClick={() => removeFigure(index)}>Remove</button> */}
-              </div>
-            ))
-          )}
-        </div>
+
+  const collectionToDisplay = Array.isArray(collection) ? collection : [];
+
+  React.useEffect(() => {
+    fetch('/api/collection')
+      .then((res) => res.json())
+      .then((figure_collection) => {
+        setCollection(figure_collection);  
+      })
+  }, []);
+
+React.useEffect(() => {
+  if (selectedBlindBox) {
+    const newFigure = { 
+      name: userName, 
+      figure: selectedBlindBox, 
+      image: selectedBlindBox.image 
+    };
+
+    fetch('/api/collection', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newFigure),
+    })
+    .then(response => response.json())
+    .then((savedFigure) => {
+      setCollection(prevCollection => [...prevCollection, savedFigure]);
+      fetch('/api/collection')  
+        .then((res) => res.json())
+        .then((data) => setCollection(data));
+    })
+  }
+}, [selectedBlindBox, userName]);
+
+return (
+  <main>
+    <h2 className="schoolbell-regular" style={{ textAlign: "center", marginBottom: "40px", color: "black" }}>
+      Your Figurine Collection
+    </h2>
+    <div className="shelf" style={{ marginTop: "100px"}}>
+      <div className="figures">
+        {collectionToDisplay.length === 0 ? (
+          <p style={{ textAlign: "center", color: "gray" }}>No figures collected yet!</p>
+        ) : (
+          collection.map((item, index) => (
+            <div key={index}>
+              <img src={item.figure.image} alt={item.figure.name} height="200" />
+            </div>
+          ))
+        )}
       </div>
-      <div className='link-container'>
-        <NavLink className="fig-handdrawn-button" to="/blindboxes">Return to Blind Boxes</NavLink>
-        <NavLink className="fig-handdrawn-button" to="/chat">Share collection with friends</NavLink>
-      </div>
-    </main>
-  );
-}
+    </div>
+    <div className='link-container'>
+      <NavLink className="fig-handdrawn-button" to="/blindboxes">Return to Blind Boxes</NavLink>
+    </div>
+  </main>
+)};
+
